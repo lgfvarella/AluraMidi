8c77fc62822e9895b69824a406bdb1d267be41a9
lgfvarella <lgfvarella@gmail.com>
2023-05-30 17:17:19 -0300

    aluraMidiFull

---
+++ main.js
@@ -0,0 +1,28 @@
+
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+function tocaSom(idDoSom) {
+	document.querySelector(idDoSom).play();
+    console.log(idDoSom)
+}
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const idDoSom = listaDeTeclas[contador].innerHTML
+    const tecla = listaDeTeclas[contador]
+
+	listaDeTeclas[contador].onclick = function () {
+        tocaSom(`#som_tecla_${idDoSom}`);
+    };
+
+    tecla.onkeydown = function (event) {
+        if(event.code === "Space" || event.code === "Enter"){
+            tecla.classList.add('ativa');
+        }
+	}
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+	}
+
+
+}
